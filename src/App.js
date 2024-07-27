fa46bf0d89823684675ccb5f96f5bc3361ccbf17
Sathiskumar2113 <sathiskumar2112000@gmail.com>
2024-07-27 13:03:37 +0530

    router create sample program

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:"hii i am sathis"
+
+    },
+    {
+      path:"/about",
+      element:"about us"
+    },
+    {
+      path:"/contact",
+      element:"contact us"
+
+    },
+    {
+      path:"/services",
+      element:"services class"
+    },
+    {
+      path:"/product",
+      element:"product services"
+    }
+
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider  
+      
+      router={router}
+      
+      ></RouterProvider>
+
     </div>
   );
 }
